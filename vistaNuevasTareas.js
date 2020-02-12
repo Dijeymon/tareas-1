d48abdbba00e712db5a6fd106576c01c44b24a92
inoriega <john.doe@company.com>
2020-02-12 17:38:17 -0300

    counters added

--- vistaNuevasTareas.js
+++ vistaNuevasTareas.js
@@ -3,13 +3,13 @@ tareas = [
     {
         textoTarea: 'Mi primer tarea',
         id: 1,
-        prioridad: 'media',
+        prioridad: 'baja',
         habilitado: 'deshabilitado'
     },
     {
         textoTarea: 'Mi segunnda tarea',
         id: 2,
-        prioridad: 'baja',
+        prioridad: 'media',
         habilitado: 'deshabilitado'
     },
     {
@@ -29,6 +29,39 @@ tareas.forEach(tarea => {
     ul.append(li);
 });
 
+// Contadores de tareas
+var contadores = [
+    document.getElementById('contador_baja'),
+    document.getElementById('contador_media'),
+    document.getElementById('contador_alta')
+];
+
+// Actualizar contadores
+contadores.forEach(contador => {
+    var counter = 0;
+    switch (contador.id) {
+        case 'contador_baja':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'baja') counter++
+            })
+            break;
+        case 'contador_media':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'media') counter++
+            })
+            break;
+        case 'contador_alta':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'alta') counter++
+            })
+            break;
+        default:
+            console.log('La prioridad ' + tarea.prioridad + ' no esta contemplada.');
+            break;
+    }
+    contador.textContent = counter;
+});
+
 // Logica de el formulario para agregar tareas
 var form = document.getElementById('formulario-agregar');
 form.addEventListener('submit', function (event) {
@@ -36,12 +69,40 @@ form.addEventListener('submit', function (event) {
     var inputText = document.getElementById('descripcion').value;
     var radio = document.querySelector('input[name="prioridad"]:checked').value;
     agregarTarea(inputText, radio);
+    
+    // Acttualizamos el listado de tareas
     var ul = document.querySelector('.lista-tareas');
     ul.innerHTML = '';
     tareas.forEach(tarea => {
         var li = document.createElement('li');
         li.textContent = tarea.textoTarea;
         ul.append(li);
+    });
+
+    // Actualizamos los contadores
+    contadores.forEach(contador => {
+        var counter = 0;
+        switch (contador.id) {
+            case 'contador_baja':
+                tareas.forEach(function(tarea) {
+                    if (tarea.prioridad === 'baja') counter++
+                })
+                break;
+            case 'contador_media':
+                tareas.forEach(function(tarea) {
+                    if (tarea.prioridad === 'media') counter++
+                })
+                break;
+            case 'contador_alta':
+                tareas.forEach(function(tarea) {
+                    if (tarea.prioridad === 'alta') counter++
+                })
+                break;
+            default:
+                console.log('La prioridad ' + tarea.prioridad + ' no esta contemplada.');
+                break;
+        }
+        contador.textContent = counter;
     });
 })
 
--- vistaTareas.js
+++ vistaTareas.js
@@ -31,6 +31,39 @@ function agregarTarea(texto, radio) {
     tareas.push(nuevaTarea);
 };
 
+// Contadores de tareas
+var contadores = [
+    document.getElementById('contador_baja'),
+    document.getElementById('contador_media'),
+    document.getElementById('contador_alta')
+];
+
+// Actualizar contadores
+contadores.forEach(contador => {
+    var counter = 0;
+    switch (contador.id) {
+        case 'contador_baja':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'baja') counter++
+            })
+            break;
+        case 'contador_media':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'media') counter++
+            })
+            break;
+        case 'contador_alta':
+            tareas.forEach(function(tarea) {
+                if (tarea.prioridad === 'alta') counter++
+            })
+            break;
+        default:
+            console.log('La prioridad ' + tarea.prioridad + ' no esta contemplada.');
+            break;
+    }
+    contador.textContent = counter;
+});
+
 // Cargar las tareas existentes
 var ul = document.querySelector('.lista-tareas');
 ul.innerHTML = '';
